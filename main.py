3fb98d2fa920eb87ebbf01f45ab2936b103faba4
Hayden Ramm <hayden.r.ramm@gmail.com>
2025-07-09 10:53:29 +0100

    4- and 5-gram models

--- main.py
+++ main.py
@@ -8,28 +8,15 @@ def main(path: str | os.PathLike = "shakespeare.txt"):
     path = path
 
     # Bigram model
-    print("\n")
-    print("BIGRAM MODEL")
-    print("------")
-    bigram = NGram(fname=path, context=2)
-    bigram.generate_text(lines_to_gen=10)
-    print("------")
+    for i in range(4, 6):
+        print("\n")
+        print(f"{i}-GRAM MODEL")
+        print("------")
+        bigram = NGram(fname=path, context=i)
+        bigram.generate_text(lines_to_gen=10)
+        print("\n")
+        print("------")
     
-    # Trigram model
-    print("\n")
-    print("TRIGRAM MODEL")
-    print("------")
-    trigram = NGram(fname=path, context=3)
-    trigram.generate_text(lines_to_gen=10)
-    print("------")
-
-    # 4gram model
-    print("\n")
-    print("4-GRAM MODEL")
-    print("------")
-    trigram = NGram(fname=path, context=4)
-    trigram.generate_text(lines_to_gen=10)
-    print("------")
 
 
 if __name__ == "__main__":
